ede2a8806cde530104c1b8cc54cebf16de4c402b
yetabalij <yetabalij@gmail.com>
2021-04-05 17:04:18 +0300

    db connection is added to the server.js file.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
+//middlewares
 const app = express();
 app.use(cors());
+app.use(express.json());
 
+//db connection
+mongoose.connect("mongodb://localhost:27017/todoapp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+var db = mongoose.connection;
+db.on("open", () => {
+  console.log("Connected to mongoDB");
+});
+db.on("error", (error) => {
+  console.log(error);
+});
+
+//start listening
 app.listen(5000, () => {
   console.log("server started on port 5000");
 });
