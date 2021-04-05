cc8f8c060037f292f1ea4b4736c0c70c921461f0
yetabalij <ermiastsegu@yahoo.com>
2021-04-05 19:38:42 +0300

    the todo backend api's are complete.

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -2,9 +2,61 @@ const Todo = require("./../models/todo");
 
 //getalltodos
 exports.getAllTodo = async (req, res) => {
-  res.send("get all to dos");
+  try {
+    const todo = await Todo.find();
+    res.status(200).json({ todo });
+  } catch (err) {
+    res.status(400).json({
+      message: err,
+    });
+  }
 };
 //getsingletodo
+exports.getSingleTodo = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const todo = await Todo.findById(id);
+    res.status(200).json({ todo });
+  } catch (err) {
+    res.status(400).json({
+      message: err,
+    });
+  }
+};
 //posttodo
+exports.postTodo = async (req, res) => {
+  const todo = req.body;
+  try {
+    await Todo.create(todo);
+    res.status(200).json({ todo });
+  } catch (err) {
+    res.status(400).json({
+      message: err,
+    });
+  }
+};
 //updatetodo
+exports.updateTodo = async (req, res) => {
+  const id = req.params.id;
+  const todo = req.body;
+  try {
+    await Todo.findByIdAndUpdate(id, todo, { useFindAndModify: false });
+    res.status(200).json("Record updated succcessfully.");
+  } catch (err) {
+    res.status(400).json({
+      message: err,
+    });
+  }
+};
 //deletetodo
+exports.deleteTodo = async(req,res)=>{
+    const id = req.params.id
+    try{
+        await Todo.findByIdAndDelete(id)
+        res.status(200).json("Record deleted successfully.")
+    }catch(err){
+        res.status(400).json({
+            message:err
+        })
+    }
+}
--- backend/routers/todo.js
+++ backend/routers/todo.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { getAllTodo } = require("./../controllers/todo");
+const {
+  getAllTodo,
+  postTodo,
+  getSingleTodo,
+  updateTodo,
+  deleteTodo,
+} = require("./../controllers/todo");
 
 const route = express.Router();
 
 route.get("/getalltodo", getAllTodo);
+route.post("/posttodo", postTodo);
+route.get("/getsingletodo/:id", getSingleTodo);
+route.put("/updatetodo/:id", updateTodo);
+route.delete("/deletetodo/:id", deleteTodo);
 
 module.exports = route;
