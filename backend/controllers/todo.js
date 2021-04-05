97e9cec799e5bc46a55da569d7140b08967ecd8c
yetabalij <ermiastsegu@yahoo.com>
2021-04-05 19:08:09 +0300

    controllers directory and file is added.

---
+++ backend/controllers/todo.js
@@ -0,0 +1,10 @@
+const Todo = require("./../models/todo");
+
+//getalltodos
+exports.getAllTodo = async (req, res) => {
+  res.send("get all to dos");
+};
+//getsingletodo
+//posttodo
+//updatetodo
+//deletetodo
